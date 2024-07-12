63512eb10ea4c146c15c16fc2f79a2a68254b3ce
Gabriel <gabrielcarneirodesouza2.0@gmail.com>
2024-07-11 23:09:03 -0300

    Desenvolvendo as classes Livro e Usuario...Criando a classe CadastroDeUsusarios

--- src/entidades/Livro.java
+++ src/entidades/Livro.java
@@ -1,6 +1,61 @@
 package entidades;
 
+import java.util.Date;
+
+import entidades.enums.Estatus;
+
 public class Livro {
 
+	private String nome;
+	private String autor;
+	private Date dataDePubliicacao;
+	private String genero;
+	private int classificacaoIndicativa;
+	private Estatus estatus;
+	
+	
+	public Livro(String nome, String autor, Date dataDePubliicacao, String genero, int classificacaoIndicativa,Estatus estatus) {
+		
+		this.nome = nome;
+		this.autor = autor;
+		this.dataDePubliicacao = dataDePubliicacao;
+		this.genero = genero;
+		this.classificacaoIndicativa = classificacaoIndicativa;
+		this.estatus = estatus;
+		
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public Date getDataDePubliicacao() {
+		return dataDePubliicacao;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public int getClassificacaoIndicativa() {
+		return classificacaoIndicativa;
+	}
+
+	public Estatus getEstatus() {
+		return estatus;
+	}
+
+
+	public void setEstatus(Estatus estatus) {
+		this.estatus = estatus;
+	}
+	
+	
+	
 	
 }
--- src/entidades/Usuario.java
+++ src/entidades/Usuario.java
@@ -1,5 +1,56 @@
 package entidades;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class Usuario {
 
+	private String nome;
+	private String cpf;
+	private String endereco;
+	private Date dataDeNascimento;
+	
+	List<Livro> livrosReservados = new ArrayList();
+	
+	
+	public Usuario(String nome, String cpf, String endereco, Date dataDeNascimento) {
+		
+		this.nome = nome;
+		this.cpf = cpf;
+		this.endereco = endereco;
+		this.dataDeNascimento = dataDeNascimento;
+		
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public Date getDataDeNascimento() {
+		return dataDeNascimento;
+	}
+
+	public void livrosReservados(Livro livro) {
+		livrosReservados.add(livro);
+	}
+	
+	
 }
