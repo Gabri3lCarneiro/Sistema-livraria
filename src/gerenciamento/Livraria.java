2b505cfd952634cce394797bb6413708c2280193
Gabri3lCarneiro <138947353+Gabri3lCarneiro@users.noreply.github.com>
2024-07-16 18:10:51 +0000

    Fazendo o tratamento de exceções

--- src/gerenciamento/Livraria.java
+++ src/gerenciamento/Livraria.java
@@ -1,5 +1,6 @@
 package gerenciamento;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,34 +15,57 @@ public class Livraria {
 	public static List<Livro> acervoDeLivros = new ArrayList();
 	
 	public void cadastroDeLivros(Livro livro) {
+		if (livro.getId().equals(acharLivroID((livro.getId())))) {
+			throw new execoesGerenciamento("ID ja existe");
+		}
 		acervoDeLivros.add(livro);
+		
 	}
 	
-	public void remocaoDeLivros(int id) {
-		acervoDeLivros.remove(acharLivro(id));
-			
+
+	public void remocaoDeLivros(String id) {
+		if(acharLivro(id) == null){
+			throw new execoesGerenciamento("ID invalido, livro nao encontrado");
 		}
-	
-	
-	public Livro acharLivro(int id){
+		acervoDeLivros.remove(acharLivro(id));
+	}
+
+
+	public String acharLivroID(String id){
 		for(Livro livro : acervoDeLivros){
-			if(livro.getId() == id){
+			if(livro.getId().equals(id)){
+				return id;
+			}
+		}
+		return null;
+	}
+
+
+	public Livro acharLivro(String id){
+		for(Livro livro : acervoDeLivros){
+			if(livro.getId().equals(id)){
 				return livro;
 			}
 		}
-		return null;//coloca as execoes em pai...
+		return null;
 	}
 
 
-	public void imprimirLivro(int id){
+	public void imprimirLivro(String id){
+		if (acharLivro(id) == null) {
+			throw new execoesGerenciamento("ID invalido, livro nao encontrado");
+		}
 		System.out.println(acharLivro(id).toStringLivraria());
 		
 	}
 	
+	
 	public  void imprimirAcervo(){
+		if (acervoDeLivros.size() == 0) {
+			throw new execoesGerenciamento("Nenhum livro cadastrado");
+		}
 		for(Livro livro : acervoDeLivros){
-			System.out.println(livro.toStringLivraria());
-
+			System.out.println(Livro.toStringLivraria);
 		}
 	}
 	
