55d9b7745e4b1059e175457e13ee322e29445a4c
Gabri3lCarneiro <138947353+Gabri3lCarneiro@users.noreply.github.com>
2024-07-16 05:56:55 +0000

    Atualizando metodos de adicao e remocao de obijetos e acrecentando metodo de busca

--- src/gerenciamento/CadastroDeUsuarios.java
+++ src/gerenciamento/CadastroDeUsuarios.java
@@ -3,6 +3,7 @@ package gerenciamento;
 import java.util.ArrayList;
 import java.util.List;
 
+import entidades.Livro;
 import entidades.Usuario;
 
 public class CadastroDeUsuarios {
@@ -10,15 +11,40 @@ public class CadastroDeUsuarios {
 	private Usuario usuaio;
 	
 	
-	List<Usuario> usuarios = new ArrayList();
+	public List<Usuario> usuarios = new ArrayList();
 	
+
+
+	public Usuario acharUsuario(String cpf){
+		for(Usuario usuario : usuarios){
+			if(usuario.getCpf().equals(cpf)){
+				return usuario;
+			}
+		}
+		return null;//coloca as execoes em pai...
+	}
+
+
+
 	
-	
-	public void cadastroDeUsuario(Usuario usuario) {
+	public void cadastrarUsuario(Usuario usuario) {
 		usuarios.add(usuario);
 	}
 	
-	public void remocaoDeUsuario(Usuario usuario) {
-		usuarios.remove(usuario);
+	public void remocaoDeUsuario(String cpf) {
+		acharUsuario(cpf);
+		usuarios.remove(acharUsuario(cpf));
+	}	
+
+	public  void imprimirUsuario(){
+		for(Usuario usuario : usuarios){
+			System.out.println(usuario.toString());
+
+		}
 	}
+
+
+
+
 }
+
--- src/gerenciamento/Livraria.java
+++ src/gerenciamento/Livraria.java
@@ -4,22 +4,46 @@ import java.util.ArrayList;
 import java.util.List;
 
 import entidades.Livro;
+import entidades.Usuario;
 
 public class Livraria {
 
 	private Livro livro;
 	
 	
-	List<Livro> livros = new ArrayList();
+	public static List<Livro> acervoDeLivros = new ArrayList();
 	
 	public void cadastroDeLivros(Livro livro) {
-		livros.add(livro);
+		acervoDeLivros.add(livro);
 	}
 	
-	public void remocaoDeLivros(Livro livro) {
-		livros.remove(livro);
+	public void remocaoDeLivros(int id) {
+		acervoDeLivros.remove(acharLivro(id));
+			
+		}
+	
+	
+	public Livro acharLivro(int id){
+		for(Livro livro : acervoDeLivros){
+			if(livro.getId() == id){
+				return livro;
+			}
+		}
+		return null;//coloca as execoes em pai...
+	}
+
+
+	public void imprimirLivro(int id){
+		System.out.println(acharLivro(id).toStringLivraria());
+		
 	}
 	
+	public  void imprimirAcervo(){
+		for(Livro livro : acervoDeLivros){
+			System.out.println(livro.toStringLivraria());
+
+		}
+	}
 	
 	
 }
