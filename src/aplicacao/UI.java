fe7e517c26636fda5cc734a9df41bf582dd57949
Gabri3lCarneiro <138947353+Gabri3lCarneiro@users.noreply.github.com>
2024-07-16 18:09:26 +0000

    Adicionando metotdo para limpar a tela

--- src/aplicacao/UI.java
+++ src/aplicacao/UI.java
@@ -36,4 +36,10 @@ public static void notaDeDevolucao(Livro livro, Usuario usuario){
 }
 
 
+public static void clearScreen() {  
+    System.out.print("\033[H\033[2J");  
+    System.out.flush();  
+} 
+
+
 }
